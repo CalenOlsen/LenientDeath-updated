92ac22da7638906fc5d320d2cfbfa589ec14a447
CalenOlsen <calenolsen@gmail.com>
2026-04-14 23:37:31 -0500

    Use released fabric-permissions-api version

--- src/main/java/red/jackf/lenientdeath/mixins/itemresilience/ServerPlayerMixin.java
+++ src/main/java/red/jackf/lenientdeath/mixins/itemresilience/ServerPlayerMixin.java
@@ -7,6 +7,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.GlobalPos;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtOps;
 import net.minecraft.nbt.Tag;
@@ -77,8 +78,8 @@ public abstract class ServerPlayerMixin extends Player implements LDGroundedPosH
     }
 
     // read grounded position
-    @Inject(method = "readAdditionalSaveData(Lnet/minecraft/nbt/CompoundTag;)V", at = @At("RETURN"))
-    private void loadGroundedPos(CompoundTag tag, CallbackInfo ci) {
+    @Inject(method = "readAdditionalSaveData(Lnet/minecraft/nbt/CompoundTag;Lnet/minecraft/core/HolderLookup$Provider;)V", at = @At("RETURN"))
+    private void loadGroundedPos(CompoundTag tag, HolderLookup.Provider provider, CallbackInfo ci) {
         if (tag.contains(LAST_GROUNDED_POS, Tag.TAG_COMPOUND))
             this.lastGroundedPos = GlobalPos.CODEC.parse(NbtOps.INSTANCE, tag.getCompound(LAST_GROUNDED_POS))
                     .resultOrPartial(LenientDeath.LOGGER::error)
@@ -86,8 +87,8 @@ public abstract class ServerPlayerMixin extends Player implements LDGroundedPosH
     }
 
     // save grounded position
-    @Inject(method = "addAdditionalSaveData(Lnet/minecraft/nbt/CompoundTag;)V", at = @At("RETURN"))
-    private void saveGroundedPos(CompoundTag tag, CallbackInfo ci) {
+    @Inject(method = "addAdditionalSaveData(Lnet/minecraft/nbt/CompoundTag;Lnet/minecraft/core/HolderLookup$Provider;)V", at = @At("RETURN"))
+    private void saveGroundedPos(CompoundTag tag, HolderLookup.Provider provider, CallbackInfo ci) {
         if (this.lastGroundedPos != null)
             GlobalPos.CODEC.encodeStart(NbtOps.INSTANCE, this.lastGroundedPos)
                     .resultOrPartial(LenientDeath.LOGGER::error)
